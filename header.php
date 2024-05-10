69554c9cf7c0976ed79350122881979d8fd2175a
KoKa241 <klimenvlad78@ukr.net>
2024-05-10 09:15:32 +0300

    Added: Bootstrap Base layout PHP check login

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php
+function checkCoockie() {
+    if (isset($_COOKIE['user'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-success p-2 fixed-top" style="--bs-bg-opacity: .5;">
+    <div class="container">
+      <a class="navbar-brand" href="#">Review Journal</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <?php
+            if (checkCoockie()) {
+                
+            }
+            else {
+                echo "<li class='nav-item'>
+                <a class='nav-link' href='register.php'>Register</a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='login.php'>Login</a>
+              </li>";
+            }
+            ?>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
--- index.php
+++ index.php
@@ -3,9 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style>
+
+    body {
+      padding-top: 60px; 
+    }
+    .footer {
+      background-color: #f8f9fa;
+      padding: 20px 0;
+      text-align: center;
+    }
+  </style>
     <title>Document</title>
 </head>
 <body>
-    <?php echo "Hello World"; ?>
+    <?php
+    require("header.php");
+    ?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 mx-auto">
+        <h2>Reviews</h2>
+        <ul class="list-group">
+          <li class="list-group-item">
+          <p class="text-start"><strong>Yulia</strong></p>  
+          Review 1
+        </li>
+          <li class="list-group-item">
+          <p class="text-start"><strong>Vlad</strong></p>    
+          Review 2
+        </li>
+          <li class="list-group-item">
+          <p class="text-start"><strong>Max</strong></p>    
+          Review 3
+        </li>
+        </ul>
+        <br>
+        <h2>Write review</h2>
+        <form>
+          <div class="form-group">
+            <label for="name">Имя</label>
+            <input type="text" class="form-control" id="name" placeholder="Введите ваше имя">
+          </div>
+          <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea class="form-control" id="message" rows="3" placeholder="Введите ваш отзыв"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Отправить</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+    <?php
+    require("footer.php");
+    ?> 
+
+
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
